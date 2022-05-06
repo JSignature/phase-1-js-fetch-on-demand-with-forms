9afba43b0492369c2790c3a141cbd8b16c589eb6
Jimmy Andreas <jimmysignature@gmail.com>
2022-05-05 21:32:47 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+//   console.log(inputForm)
+inputForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const input = e.target.children[1].value
+    console.log(input)
+fetch(`http://localhost:3000/movies/${input}`)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json.summary)
+    const title = document.querySelector("#movieDetails h4")
+    console.log(title)
+    const summary = document.querySelector("#movieDetails p")
+    console.log(summary)
+    title.innerText = json.title
+    summary.innerText = json.summary
+    
+    
+    })
+
+
+
+
+})
 }
 
 document.addEventListener('DOMContentLoaded', init);
